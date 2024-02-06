7983099663c009e6530b4d9d75b70d35b2eb5729
grievous0 <jvopereira2004@gmail.com>
2024-02-06 01:11:00 -0300

    fix: change card to button

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,6 +1,6 @@
 export function NoteCard() {
     return (
-        <div className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
+        <button className='text-left rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
             <span className='font-medium text-sm text-slate-300'>
                 Há 2 dias
             </span>
@@ -9,6 +9,6 @@ export function NoteCard() {
             </p>
 
             <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'/>
-        </div>
+        </button>
     )
 }
