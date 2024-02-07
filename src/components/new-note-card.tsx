58fc078486887cda000228cbfb83e50fd8fe854e
grievous0 <jvopereira2004@gmail.com>
2024-02-07 19:57:41 -0300

    feat: add search and localStorage

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,8 +3,11 @@ import { X } from 'lucide-react'
 import { ChangeEvent, FormEvent, useState } from 'react'
 import { toast } from 'sonner'
 
+interface NewNoteCardProps {
+    onNoteCreated: (content: string) => void
+}
 
-export function NewNoteCard() {
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
     const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
     const [content, setContent] = useState('')
 
@@ -20,6 +23,12 @@ export function NewNoteCard() {
 
     function handleSaveNote(event: FormEvent) {
         event.preventDefault()
+
+        onNoteCreated(content)
+
+        setContent('')
+        setShouldShowOnboarding(true)
+
         toast.success('Nota criada com sucesso')
     }
 
@@ -56,6 +65,7 @@ export function NewNoteCard() {
                                     autoFocus 
                                     className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none'
                                     onChange={handleContentChanged}
+                                    value={content}
                                 />
                             )}
                         </div>
